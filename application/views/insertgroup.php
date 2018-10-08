f65571e795485b3fd5d67a7df2a2d6fe415f0800
kimsinwu <kimweir9@gmail.com>
2018-10-08 14:14:53 +0700

    interface insertGroup and insertHerb

---
+++ application/views/insertgroup.php
@@ -0,0 +1,9 @@
+<div class="container">
+    <h1>Insert Herb</h1>
+<form>
+  <div class="form-group">
+    <label for="formGroupExampleInput">ชื่อกลุ่มอาการ</label>
+    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="input">
+  </div>
+  <button type="button" class="btn btn-primary">Insert</button>
+</div>  
