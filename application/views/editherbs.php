9dffd36157d870ac3754a2ccb66dc173cd852d1a
firsticz <thanawat6368@gmail.com>
2018-10-10 20:02:55 +0700

    edit title

--- application/views/editherbs.php
+++ application/views/editherbs.php
@@ -9,7 +9,7 @@
     crossorigin="anonymous">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB"
     crossorigin="anonymous">
-  <title>Bootstrap Theme</title>
+  <title>Edit Herb</title>
 </head>
 
 <body>
