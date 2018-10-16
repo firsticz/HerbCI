90adad98916218196c66e55da8e7ce422dc8aa50
kimsinwu <kimweir9@gmail.com>
2018-10-16 16:48:18 +0700

    Add insertDrug

--- application/views/insertdrug.php
+++ application/views/insertdrug.php
@@ -1,106 +1,119 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <body>
-    <div class="container">  
-    <div class="row">
-        <div class="col-md-5">
-        <div class="form-group">
-                <h1>Insert Drug Formula</h1>
-                <label >Name Drug</label><br>
-                <input type="text" class="form-control" id="Drugname"><br>
-                <div class="form-row">
-                <label >Recipe Type</label><br>
-                <input type="text" class="form-control" id="Recipetype"><br>   
-                <div data-role="dynamic-fields">
-                <div class="form-inline">
-                <label >Herb</label><br>
-                <textarea type="text" class="form-control" id="field-name" rows="4" cols="50"  placeholder="Herb"></textarea><br><br>
-                <label >Weight</label><br>
-                <input type="text" class="form-control"  id="field-name"  id="weight" >
-                <button class="btn btn-danger" data-role="remove">
-                        <span class="glyphicon glyphicon-remove"></span>
-                    </button>
-                    <button class="btn btn-primary" data-role="add">
-                        <span class="glyphicon glyphicon-plus"></span>
-                    </button><br><br>  
-            </div>  <!-- /div[data-role="dynamic-fields"] -->
-        </div>  <br><!-- /div.col-md-12 -->
-        <div data-role="dynamic-fields">
-                <div class="form-inline">
-         <label >Suggestion </label><br>
-            <textarea type="text" class="form-control" id="field-name" rows="4" cols="50"  placeholder="Suggestion"></textarea>
-            <button class="btn btn-danger" data-role="remove">
-                        <span class="glyphicon glyphicon-remove"></span>
-                    </button>
-                    <button class="btn btn-primary" data-role="add">
-                        <span class="glyphicon glyphicon-plus"></span>
-                    </button>
-                    </div>  <!-- /div[data-role="dynamic-fields"] -->
-        </div>  <br><br><!-- /div.col-md-12 -->
-        <div data-role="dynamic-fields">
-                <div class="form-inline">
-         <label >Use </label><br>
-            <textarea type="text" class="form-control" id="field-name" rows="4" cols="50"  placeholder="Use"></textarea>
-            <button class="btn btn-danger" data-role="remove">
-                        <span class="glyphicon glyphicon-remove"></span>
-                    </button>
-                    <button class="btn btn-primary" data-role="add">
-                        <span class="glyphicon glyphicon-plus"></span>
-                    </button>
-                    </div>  <!-- /div[data-role="dynamic-fields"] -->
-        </div>  <br><br><!-- /div.col-md-12 -->
-        <div data-role="dynamic-fields">
-                <div class="form-inline">
-         <label >Warning</label><br>
-            <textarea type="text" class="form-control" id="field-name" rows="4" cols="50"  placeholder="Warning"></textarea>
-            <button class="btn btn-danger" data-role="remove">
-                        <span class="glyphicon glyphicon-remove"></span>
-                    </button>
-                    <button class="btn btn-primary" data-role="add">
-                        <span class="glyphicon glyphicon-plus"></span>
-                    </button>
-                    </div>  <!-- /div[data-role="dynamic-fields"] -->
-        </div>  <br><br><!-- /div.col-md-12 -->
-        <div data-role="dynamic-fields">
-                <div class="form-inline">
-         <label >Side Effect</label><br>
-            <textarea type="text" class="form-control" id="field-name" rows="4" cols="50"  placeholder="Side Effect"></textarea>
-            <button class="btn btn-danger" data-role="remove">
-                        <span class="glyphicon glyphicon-remove"></span>
-                    </button>
-                    <button class="btn btn-primary" data-role="add">
-                        <span class="glyphicon glyphicon-plus"></span>
-                    </button>
-                    </div>  <!-- /div[data-role="dynamic-fields"] -->
-        </div>  <br><br><!-- /div.col-md-12 -->
-        <br><button type="button" class="btn btn-primary">Insert</button> 
-    </div>  <!-- /div.row -->
-    </div>
-</div>	<script type="text/javascript">
-	$(function() {
-    // Remove button click
-    $(document).on(
-        'click',
-        '[data-role="dynamic-fields"] > .form-inline [data-role="remove"]',
-        function(e) {
-            e.preventDefault();
-            $(this).closest('.form-inline').remove();
-        }
-    );
-    // Add button click
-    $(document).on(
-        'click',
-        '[data-role="dynamic-fields"] > .form-inline [data-role="add"]',
-        function(e) {
-            e.preventDefault();
-            var container = $(this).closest('[data-role="dynamic-fields"]');
-            new_field_group = container.children().filter('.form-inline:first-child').clone();
-            new_field_group.find('input').each(function(){
-                $(this).val('');
+<div class="container">  
+<div class="row">
+<div class="col-md-5">
+<h1>Insert Drug Formula</h1>
+<label >Name Drug</label><br>
+<input type="text" class="form-control" id="drugname" name="drugname"><br>
+<label >Recipe Type</label><br>
+<input type="text" class="form-control" id="recipetype" name="recipetype"><br>
+<label >Side Effect</label><br>
+<textarea type="text" class="form-control" id="sideEffect" rows="4" cols="50"  name="sideEffect"></textarea><br>
+
+<span id="temp1"></span>
+<span id="temp2"></span>
+<span id="temp3"></span>
+<script>
+$(document).ready(function(){
+    var str = "";
+    str = 
+            '<p class="herb1">Herb Name<textarea class="form-control herb" rows="1" cols="50">'
+			+'</textarea>Weight<textarea class="form-control weight" rows="1" cols="50">'
+			+'</textarea></p>';
+
+            $("#temp1").append(str);
+    str = '<p class="use">Use<textarea class="form-control use" rows="4" cols="50">'
+			+'</textarea></p>';
+
+            $("#temp2").append(str);
+    str = '<p class="warning">Warning<textarea class="form-control warning" rows="4" cols="50">'
+			+'</textarea></p>';
+
+			$("#temp3").append(str);
+    $("#create").click(function(){
+          str = 
+            '<p class="herb1">Herb Name<textarea class="form-control herb" rows="1" cols="50">'
+			+'</textarea>Weight<textarea class="form-control weight" rows="1" cols="50">'
+			+'</textarea></p>';
+
+			$("#temp1").append(str);
+    });       
+    $("#removeHerb").click(function(){
+            $('.herb1').remove(); 
+    });
+
+    $("#createUse").click(function(){
+        str = '<p class="use">Use<textarea class="form-control use" rows="4" cols="50">'
+			+'</textarea></p>';
+
+			$("#temp2").append(str);
+    });
+    $("#removeUse").click(function(){
+            $('.use').remove(); 
+    });
+
+    $("#createWarning").click(function(){
+        str = '<p class="warning">Warning<textarea class="form-control warning" rows="4" cols="50">'
+			+'</textarea></p>';
+
+			$("#temp3").append(str);
+    });
+    $("#removeWarning").click(function(){
+            $('.warning').remove(); 
+    });
+
+    $("#insert").click(function(){
+    	var obj = {
+            "drugname" : $("#drugname").val(),
+            "recipetype" : $("#recipetype").val(),
+            "herb" : null,
+            "use":null,
+            "warning":null
+
+        };
+    	var arr = [];
+        $( ".herb1" ).each(function( index ) {
+        	//arr[index] = $( this ).val();
+            var name = $( this ).children(".herb").val();
+            var weight = $( this ).children(".weight").val();
+            arr.push({
+            	"name": name,
+                "weight": weight
             });
-            container.append(new_field_group);
-        }
-    );
+        });
+        obj.herb = arr;
+
+        var arr = [];
+        $( ".use" ).each(function( index ) {
+        	arr[index] = $( this ).val();
+        });
+        obj.use = arr;
+
+        var arr = [];
+        $( ".warning" ).each(function( index ) {
+        	arr[index] = $( this ).val();
+        });
+        obj.use = arr;
+
+        console.log(obj); //ติด
+
+        $.post("http://localhost:8080/Mhunpris/api/drugformula/insert", JSON.stringify(obj),//ติด
+            function (data, textStatus, jqXHR) {
+                alert(data.message);
+            }
+        );
+    })
 });
-	</script>
+</script>
+</head>
+<body>
+<button id="create" class="btn">Insert Herb</button>   <button id="removeHerb" class="btn">Remove Herb</button><br><br><br>
+<button id="createUse" class="btn">Insert Use</button>  <button id="removeUse" class="btn">Remove Use</button><br><br><br>
+<button id="createWarning" class="btn">Insert Warning</button>  <button id="removeWarning" class="btn">Remove Warning</button><br><br><br>
+<br><button type="button" class="btn btn-primary" id="insert" >Insert</button> 
 </body>
 </html>
- 
