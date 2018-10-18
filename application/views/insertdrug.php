361ba10975dde738193c8b07d4ecaf43c81104a5
Wichuta18 <kimweir9@gmail.com>
2018-10-19 01:26:38 +0700

    Edit UI and Design

--- application/views/insertdrug.php
+++ application/views/insertdrug.php
@@ -1,49 +1,34 @@
-<body>
-<div class="container">  
-<div class="row">
-<div class="col-md-5">
-<h1>Insert Drug Formula</h1>
-<label >Name Drug</label><br>
-<input type="text" class="form-control" id="drugName" name="drugName"><br>
-<label >Recipe Type</label><br>
-<input type="text" class="form-control" id="recipetype" name="recipetype"><br>
-<!-- <label >Side Effect</label><br>
-<textarea type="text" class="form-control" id="sideEffect" rows="4" cols="50"  name="sideEffect"></textarea><br> -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?php echo base_url() ?>assets/css/style.css">
+    <script src="<?php echo base_url() ?>assets/js/jquery-3.3.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="<?php echo base_url() ?>assets/js/setup.js"></script>
+    <!-- <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script> -->
+    <link href="http://cdn.phpoll.com/css/animate.css" rel="stylesheet">
+   
+    <title>Insert Drug Formula</title>
 
-<span id="temp1"></span>
-<span id="temp2"></span>
-<span id="temp3"></span>
-<span id="temp3"></span>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
 <script>
 $(document).ready(function(){
     var str = "";
-    str = 
-            '<p class="herb"><label>Herb Name</label><textarea type="text" class="form-control herbname" rows="1" cols="50">'
-			+'</textarea><label>Weight</label><textarea class="form-control weight" rows="1" cols="50">'
-			+'</textarea></p>'; 
-
-            $("#temp1").append(str);
-    
-    str = '<p class="suggestion"><label>Suggestion</label><textarea type="text" class="form-control suggestion1" rows="4" cols="50">'
-			+'</textarea></p>';
-
-			$("#temp3").append(str);
-            
-    str = '<p class="use"><label>Use</label><textarea type="text" class="form-control use1" rows="4" cols="50">'
-			+'</textarea></p>';
-
-            $("#temp2").append(str);
-
-    str = '<p class="warning"><label>Warning</label><textarea type="text" class="form-control warning1" rows="4" cols="50">'
-			+'</textarea></p>';
-
-			$("#temp3").append(str);
     $("#create").click(function(){
           str = 
-            '<p class="herb"><label>Herb Name*</label><textarea type="text" class="form-control herbname" rows="1" cols="50">'
-			+'</textarea><label>Weight*</label><textarea class="form-control weight" rows="1" cols="50">'
-			+'</textarea></p>';
+            '<p class="herb"><label>สมุนไพร*</label>'+
+            '<input type="text" class="form-control herbname"  placeholder="กรอกสมุนไพร..">'+
+            '<label >น้ำหนัก*</label>'+
+            '<input type="text" class="form-control weight"  placeholder="กรอกน้ำหนักของสมุนไพร(กรัม)..">';
 
             $("#temp1").append(str);
             
@@ -53,8 +38,8 @@ $(document).ready(function(){
     });
 
     $("#createSuggestion").click(function(){
-        str = '<p class="suggestion"><label>Suggestion</label><textarea type="text" class="form-control suggestion1" rows="4" cols="50">'
-			+'</textarea></p>';
+        str = '<label for="suggestion">สรรพคุณ*</label>'+
+                '<input type="text" class="form-control suggestion" id="suggestion" placeholder="กรอกสรรพคุณของสูตรยา..">';
 
 			$("#temp3").append(str);
     });
@@ -63,20 +48,20 @@ $(document).ready(function(){
     });
 
     $("#createUse").click(function(){
-        str = '<p class="use"><label>Use*</label><textarea type="text" class="form-control use1" rows="4" cols="50">'
-			+'</textarea></p>';
+        str = '<label for="use">วิธ๊ใช้*</label>'+
+                '<input type="text" class="form-control use" id="use" placeholder="กรอกวิธีใช้ของสูตรยา..">';
 
-			$("#temp2").append(str);
+			$("#temp4").append(str);
     });
     $("#removeUse").click(function(){
             $('.use').remove(); 
     });
 
     $("#createWarning").click(function(){
-        str = '<p class="warning"><label>Warning*</label><textarea type="text" class="form-control warning1" rows="4" cols="50">'
-			+'</textarea></p>'; 
+        str = '<label for="warning">คำเตือน*</label>'+
+                '<input type="text" class="form-control warning" id="warning" placeholder="กรอกคำเตือน..">'; 
 
-			$("#temp3").append(str);
+			$("#temp5").append(str);
     });
     $("#removeWarning").click(function(){
             $('.warning').remove(); 
@@ -105,19 +90,19 @@ $(document).ready(function(){
         obj.herb = arr;
 
         var arr = [];
-        $( ".suggestion1" ).each(function( index ) {
+        $( ".suggestion" ).each(function( index ) {
         	arr[index] = $( this ).val();
         });
         obj.suggestion = arr;
 
         var arr = [];
-        $( ".use1" ).each(function( index ) {
+        $( ".use" ).each(function( index ) {
         	arr[index] = $( this )  .val();
         });
         obj.use = arr;
 
         var arr = [];
-        $( ".warning1" ).each(function( index ) {
+        $( ".warning" ).each(function( index ) {
         	arr[index] = $( this ).val();
         });
         obj.warning = arr;
@@ -133,12 +118,148 @@ $(document).ready(function(){
     })
 });
 </script>
-
+</head>
 <body>
-<button id="create" class="btn btn-success">Insert Herb</button>   <button id="removeHerb" class="btn btn-danger">Remove Herb</button><br><br><br>
-<button id="createSuggestion" class="btn btn-success">Insert Suggestion</button>   <button id="removeSuggestion" class="btn btn-danger">Remove Suggestion</button><br><br><br>
-<button id="createUse" class="btn btn-success">Insert Use</button>  <button id="removeUse" class="btn btn-danger">Remove Use</button><br><br><br>
-<button id="createWarning" class="btn btn-success">Insert Warning</button>  <button id="removeWarning" class="btn btn-danger">Remove Warning</button><br><br><br>
-<br><button type="button" class="btn btn-primary btn-lg" id="insert" >Insert</button> 
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top" id="navlogin">
+       
+    </nav>
+
+
+
+    <div class="container">
+
+    <!-- Page Heading/Breadcrumbs -->
+    <h1 class="mt-4 mb-3">Herb
+    <small>Insert</small>
+    </h1>
+
+    <ol class="breadcrumb">
+    <li class="breadcrumb-item">
+        <a href="index2">Home</a>
+    </li>
+    <li class="breadcrumb-item active">Herb Insert</li>
+    </ol>
+
+    <div class="card mb-4">
+        <div class="card-body">
+          <div class="row">
+            <div class="col-lg-12 ">
+            <form>
+                    <div class="row">
+                        <div class="form-group col-md-6">
+                            <label for="drugName">ชื่อสูตรยา</label>
+                            <input type="text" class="form-control drugName" id="drugName" placeholder="กรอกชื่อสูตรยา..">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="form-group col-md-6">
+                            <label for="recipetype">ประเภทสูตรยา</label>
+                            <input type="text" class=" form-control recipetype" id="recipetype" placeholder="กรอกประเภทสูตรยา..">
+                           
+                           
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="form-group col-md-6">
+                            <p class="herb"><label>สมุนไพร</label>
+                            <input type="text" class="form-control herbname"  placeholder="กรอกสมุนไพร..">
+                            <label >น้ำหนัก</label>
+                            <input type="text" class="form-control weight"  placeholder="กรอกน้ำหนักของสมุนไพร(กรัม)..">
+                            <span id="temp1"></span><br>
+                            <button type="button" id="create" class="btn btn-secondary btn-sm">เพิ่มสมุนไพร</button>
+                            <button type="button" id="removeHerb" class="btn btn-secondary btn-sm">Remove</button>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="form-group col-md-6">
+                            <label for="suggestion">สรรพคุณ</label>
+                            <input type="text" class="form-control suggestion" id="suggestion" placeholder="กรอกสรรพคุณของสูตรยา..">
+                            <span id="temp3"></span><br>
+                            <button type="button" id="createSuggestion" class="btn btn-secondary btn-sm">เพิ่มสรรพคุณ</button>
+                            <button type="button" id="removeSuggestion" class="btn btn-secondary btn-sm">Remove</button>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="form-group col-md-6">
+                            <label for="use">วิธ๊ใช้</label>
+                            <input type="text" class="form-control use" id="use" placeholder="กรอกวิธีใช้ของสูตรยา..">
+                            <span id="temp4"></span><br>
+                            <button type="button" id="createUse" class="btn btn-secondary btn-sm">เพิ่มวิธีใช้</button>
+                            <button type="button" id="removeUse" class="btn btn-secondary btn-sm">Remove</button>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="form-group col-md-6">
+                            <label for="warning">คำเตือน</label>
+                            <input type="text" class="form-control warning" id="warning" placeholder="กรอกคำเตือน..">
+                            <span id="temp5"></span><br>
+                            <button type="button" id="createWarning" class="btn btn-secondary btn-sm">เพิ่มคำเตือน</button>
+                            <button type="button" id="removeWarning" class="btn btn-secondary btn-sm">Remove</button>
+                        </div>
+                    </div>
+
+                    <button type="button" id="insert" class="btn btn-primary">บันทึก</button>
+                </form>
+            </div>
+          </div>
+        </div>
+        <!-- <div class="card-footer text-muted">
+         อ้างอิงจากบัญชียาหลักแห่งชาติ
+          
+        </div> -->
+      </div>
+
+</div>
+</div>
+</div>
+<footer class="py-5 bg-dark text-white">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <h5><i class="fas fa-leaf"></i> Mhunprai</h5>
+                        <div class="row">
+                            <div class="col-6">
+                                <ul class="list-unstyled">
+                                    <li><a href="">Product</a></li>
+                                    <li><a href="">Benefits</a></li>
+                                    <li><a href="">Partners</a></li>
+                                    <li><a href="">Team</a></li>
+                                </ul>
+                            </div>
+                            <div class="col-6">
+                                <ul class="list-unstyled">
+                                    <li><a href="">Documentation</a></li>
+                                    <li><a href="">Support</a></li>
+                                    <li><a href="">Legal Terms</a></li>
+                                    <li><a href="">About</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <ul class="nav">
+                            <li class="nav-item"><a href="" class="nav-link pl-0"><i class="fab fa-facebook fa-lg"></i></a></li>
+                            <li class="nav-item"><a href="" class="nav-link"><i class="fab fa-twitter fa-lg"></i></a></li>
+                            <li class="nav-item"><a href="" class="nav-link"><i class="fab fa-github fa-lg"></i></a></li>
+                            <li class="nav-item"><a href="" class="nav-link"><i class="fab fa-instagram fa-lg"></i></a></li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-6">
+                        <h5 class="text-md-right">Contact Us</h5>
+                        <hr>
+                        <form>
+                            <fieldset class="form-group">
+                                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                            </fieldset>
+                            <fieldset class="form-group">
+                                <textarea class="form-control" id="exampleMessage" placeholder="Message"></textarea>
+                            </fieldset>
+                            <fieldset class="form-group text-xs-right">
+                                <button type="button" class="btn btn-secondary-outline btn-lg">Send</button>
+                            </fieldset>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <script src="<?php echo base_url() ?>assets/js/navlogin.js"></script>
 </body>
-</html>
+</html>     
